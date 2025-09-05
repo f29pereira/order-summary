b8ec5222bc4fa94ff518927a27743314bcc2a589
Filipe Pereira <f29.pereira@gmail.com>
2025-09-05 15:00:32 +0100

    Configure Next.js for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/order-summary",
+  assetPrefix: "/order-summary/",
 };
 
 export default nextConfig;
