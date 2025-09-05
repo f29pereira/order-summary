2c5923e3016e1a867ee27386d92229c335becbb4
Filipe Pereira <f29.pereira@gmail.com>
2025-09-05 14:19:17 +0100

    Add images, styling, page font, TS types and Card component
    - Add images to be used in order summary page
    - Add styling to globals.css and page.modules.css
    - Add font: Read Hat Display
    - Add index.ts for TypeScript types
    - Add Card component and Card.module.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
+
+const redHatDisplay = Red_Hat_Display({
+  variable: "--font-red-hat-display",
+  subsets: ["latin"],
+  weight: ["500", "700", "900"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <title>Order Summary</title>
+      <body className={`${redHatDisplay.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Card from "./components/Card/Card";
 
 export default function Home() {
-  return <div>Home component</div>;
+  return (
+    <main className={`flex-full-center ${styles.background}`}>
+      <section className={styles.paddingX}>
+        <Card
+          imgSrc="./images/illustration-hero.svg"
+          imgAlt="Person listening to music and dancing"
+          title="Order Summary"
+          description="You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!"
+        />
+      </section>
+    </main>
+  );
 }
